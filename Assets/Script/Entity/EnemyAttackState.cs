9714dc639a03c811b1d727e34be43ffcb139a402
Korpo-spec <casper22x@gmail.com>
2024-04-11 12:15:06 +0200

    I'm going for lunch state machine on its way

--- Assets/Script/Entity/EnemyAttackState.cs
+++ Assets/Script/Entity/EnemyAttackState.cs
@@ -6,4 +6,36 @@ using UnityEngine;
 public class EnemyAttackState : State
 {
     
+    private StateController _controller;
+    private Collider[] _colliders;
+    [SerializeField] private LayerMask _mask;
+    [SerializeField] private float _radius;
+    [SerializeField] private Faction _factionToAttack;
+    [SerializeField] private State _stateToTransistion;
+
+    public override void OnEnter(StateController controller)
+    {
+        _controller = controller;
+        _colliders = new Collider[15];
+    }
+
+    public override void UpdateState()
+    {
+        int amount = Physics.OverlapSphereNonAlloc(_controller.transform.position, _radius, _colliders, _mask);
+
+        for (int i = 0; i < amount; i++)
+        {
+            if (_colliders[i].TryGetComponent<EntityFaction>(out var faction))
+            {
+                if (faction.faction == _factionToAttack)
+                {
+                    _controller.Transistion(_stateToTransistion);
+                }
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
 }
