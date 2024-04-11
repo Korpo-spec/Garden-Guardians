fa4e3f5deaa3372257d022edfeed6a293fc7fbf6
Korpo-spec <casper22x@gmail.com>
2024-04-11 15:07:14 +0200

    First iteration enemy

--- Assets/Script/Entity/EnemyAttackState.cs
+++ Assets/Script/Entity/EnemyAttackState.cs
@@ -9,9 +9,13 @@ public class EnemyAttackState : State
     
     private StateController _controller;
     private EntityMovement _movement;
+    private Animator _animator;
     private Collider[] _colliders;
+    private float _time;
     [SerializeField] private LayerMask _mask;
     [SerializeField] private float _radius;
+    [SerializeField] private float _attackRange;
+    [SerializeField] private float _attackSpeed;
     [SerializeField] private Faction _factionToAttack;
     [SerializeField] private State _stateToTransistion;
     [SerializeField] private AttackModule _attackModule;
@@ -23,6 +27,7 @@ public class EnemyAttackState : State
         _controller = controller;
         _colliders = new Collider[15];
         _movement = controller.GetComponent<EntityMovement>();
+        _animator = controller.GetComponent<Animator>();
         int amount = Physics.OverlapSphereNonAlloc(_controller.transform.position, _radius, _colliders, _mask);
 
         for (int i = 0; i < amount; i++)
@@ -45,7 +50,22 @@ public class EnemyAttackState : State
 
     public override void UpdateState()
     {
-        _movement.SetDestinationTo(_target.position.RemoveY());
-        _movement.UpdateMoveTo();
+        if (_attackSpeed > _time)
+        {
+            _time += Time.deltaTime;
+            return;
+        }
+        
+        if (_attackRange >= Vector3.Distance(_controller.transform.position.RemoveY(), _target.position.RemoveY()))
+        {
+            _attackModule.Attack(_animator);
+            _time = 0;
+        }
+        else
+        {
+            _movement.SetDestinationTo(_target.position.RemoveY());
+            _movement.UpdateMoveTo();
+        }
+        
     }
 }
