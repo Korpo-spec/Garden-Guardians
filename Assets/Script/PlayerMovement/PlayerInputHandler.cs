96415e767552cbb08e4e2ecade1bfd73a802dfe3
Erzer1 <simsonandersson@yahoo.se>
2024-04-11 16:00:05 +0200

    Added stamina points to PlayerInputHandler

--- Assets/Script/PlayerMovement/PlayerInputHandler.cs
+++ Assets/Script/PlayerMovement/PlayerInputHandler.cs
@@ -13,26 +13,69 @@ namespace Script.PlayerMovement
         [HideInInspector]public bool isDashing;
 
         [HideInInspector] public bool attackButtonPressed;
+
+        [SerializeField] public int staminaPoints;
+
+        [SerializeField] private int staminaPointMax;
+
+        private int timer;
+        private int resetTime = 390;
+
+        [SerializeField] private GameObject point1;
+
+        [SerializeField] private GameObject point2;
     
         // Start is called before the first frame update
         void Start()
         {
-        
+            staminaPointMax = 2;
+            staminaPoints = staminaPointMax;
+            timer = resetTime;
         }
 
         // Update is called once per frame
         void Update()
         {
-        
+            if (staminaPoints < staminaPointMax)
+            {
+                timer--;
+            }
+
+            if (timer <= 0 && staminaPoints < staminaPointMax)
+            {
+                staminaPoints++;
+                timer = resetTime;
+            }
+            
+            // Show how many stamina point you have
+            if (staminaPoints > 0)
+            {
+                point1.SetActive(true);
+            }
+            else
+            {
+                point1.SetActive(false);
+            }
+            
+            if (staminaPoints > 1)
+            {
+                point2.SetActive(true);
+            }
+            else
+            {
+                point2.SetActive(false);
+            }
         }
 
         public void GetPlayerInput()
         {
             GetStandardMovementInput(Camera.main.transform.rotation);  
 
-            if (Input.GetKeyDown(dashKey))
+            if (Input.GetKeyDown(dashKey) && staminaPoints > 0)
             {
                 isDashing = true;
+                staminaPoints--;
+                timer = resetTime;
             }
             if (Input.GetMouseButton(0))
             {
