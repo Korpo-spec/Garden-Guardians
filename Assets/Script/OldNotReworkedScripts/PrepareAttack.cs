f83e11a17641bb1925690659bfa8a6eb2815a13c
Korpo-spec <casper22x@gmail.com>
2024-04-11 15:48:28 +0200

    Prepare attack working

---
+++ Assets/Script/OldNotReworkedScripts/PrepareAttack.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PrepareAttack : MonoBehaviour
+{
+    [SerializeField] private float _attackWindup;
+    [SerializeField] private GameObject meshParent;
+    
+    private Material _material;
+    private Animator _animator;
+    private NavMeshAgent _agent;
+    private float _time;
+    private Texture emissionTexture;
+    private void Start()
+    {
+        _material = GetCommonMaterial();
+        _animator = GetComponent<Animator>();
+        _agent = GetComponent<NavMeshAgent>();
+        emissionTexture = _material.GetTexture("_EmissionMap");
+        
+    }
+
+    private Material GetCommonMaterial()
+    {
+        Renderer[] renderers = meshParent.GetComponentsInChildren<Renderer>();
+        Material mat = renderers[0].material;
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            renderers[i].material = mat;
+        }
+
+        return mat;
+    }
+    
+    public void StartPrepareAttack()
+    {
+        StartCoroutine(WindUpAttack());
+    }
+
+    private IEnumerator WindUpAttack()
+    {
+        _time = 0;
+        _animator.speed = 0;
+        _agent.isStopped = true;
+        
+        _material.SetTexture("_EmissionMap", Texture2D.whiteTexture);
+        _material.SetColor("_EmissionColor", Color.black);
+        while (1 > _time)
+        {
+            _time += Time.deltaTime * (1/_attackWindup);
+            Color color = Color.Lerp(Color.black, Color.white, _time);
+            _material.SetColor("_EmissionColor", color);
+            _material.EnableKeyword("_EMISSION");
+            yield return new WaitForEndOfFrame();
+        }
+        _animator.speed = 1;
+         _material.SetTexture("_EmissionMap", emissionTexture);
+        _material.SetColor("_EmissionColor", Color.black);
+    }
+}
