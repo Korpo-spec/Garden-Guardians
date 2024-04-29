7438481c3dceb329fd0ac5b23f6b5eeb77bc2956
Breeake <123238518+Breeake@users.noreply.github.com>
2024-04-29 13:37:53 +0200

    Fixed fungus triggering multiple times

--- Assets/Assets/Script/CharacterTriggerScript.cs
+++ Assets/Assets/Script/CharacterTriggerScript.cs
@@ -8,17 +8,35 @@ public class CharacterTriggerScript : MonoBehaviour
 {
     private Character character;
     [SerializeField] private KeyCode interactKey;
+    private bool canTrigger;
     
     private void Start()
     {
         character = gameObject.GetComponent<Character>();
+        canTrigger = true;
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (Input.GetKeyDown(interactKey))
+        if (Input.GetKeyDown(interactKey) && canTrigger)
         {
             Fungus.Flowchart.BroadcastFungusMessage(character.NameText + "Start");
+        }
+    }
+
+    public void ChangeTrigger()
+    {
+        if (canTrigger)
+        {
+            canTrigger = false;
+        }
+        else if (!canTrigger)
+        {
+            canTrigger = true;
+        }
+        else
+        {
+            return;
         }
     }
 }
