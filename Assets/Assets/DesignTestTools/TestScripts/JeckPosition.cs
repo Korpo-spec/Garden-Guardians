b25df3cc6c49c92bad499bdf9cce21c27e43c1f1
Erzer1 <simsonandersson@yahoo.se>
2024-05-02 11:00:32 +0200

    Gave Jeck teleport positions in his script

--- Assets/Assets/DesignTestTools/TestScripts/JeckPosition.cs
+++ Assets/Assets/DesignTestTools/TestScripts/JeckPosition.cs
@@ -8,9 +8,18 @@ public class JeckPosition : MonoBehaviour
     [SerializeField] private float startPosX;
     [SerializeField] private float startPosZ;
     
-    // New position
-    [SerializeField] private float newPosX = 306.41f;
-    [SerializeField] private float newPosZ = 231.74f;
+    // Safe pos 1
+    [SerializeField] private float safePos1X = 306.41f;
+    [SerializeField] private float safePos1Z = 231.74f;
+    
+    // Safe pos 2
+    [SerializeField] private float safePos2X = 439.95f;
+    [SerializeField] private float safePos2Z = 545.46f;
+    
+    // Safe pos 3
+    [SerializeField] private float safePos3X = 90.65f;
+    [SerializeField] private float safePos3Z = 107.71f;
+    
     
     
     void Start()
