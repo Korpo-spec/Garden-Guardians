17246a2023f0cc018543ffe5af0ab5b5be1cd189
Erzer1 <simsonandersson@yahoo.se>
2024-05-02 17:03:01 +0200

    Added clear prompt to unlock shortcuts, to canvas
    Altered UnlockShortcut script to make it work

--- Assets/Assets/DesignTestTools/TestScripts/UnlockShortcut.cs
+++ Assets/Assets/DesignTestTools/TestScripts/UnlockShortcut.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 
 public class UnlockShortcut : MonoBehaviour
 {
+    [SerializeField] private GameObject prompt;
     void Start()
     {
         Collider collider = GetComponent<Collider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        prompt.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        prompt.SetActive(false);
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
+            prompt.SetActive(false);
             Destroy(gameObject);
         }
     }
