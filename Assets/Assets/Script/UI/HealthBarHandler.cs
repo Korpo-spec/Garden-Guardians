57a5c43efb153c481cec6ee73dafa6b1911177ff
Taqiqi <98962033+Taqiqi@users.noreply.github.com>
2024-05-20 10:14:05 +0200

    HealthBars

---
+++ Assets/Assets/Script/UI/HealthBarHandler.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Script.Entity;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+public class HealthBarHandler : MonoBehaviour
+{
+    public Slider healthSlider;
+    public Slider healthEaseSlider;
+    public EntityHealth EntityHealth;
+    private float lerpSpeed=0.05f;
+
+    private Camera mainCamera;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void Start()
+    {
+        healthSlider.value = EntityHealth.health / EntityHealth.maxHealth;
+    }
+
+    private void LateUpdate()
+    {
+        
+        healthEaseSlider.value = Mathf.Lerp(healthEaseSlider.value, healthSlider.value, lerpSpeed);
+        transform.LookAt(transform.position+mainCamera.transform.forward);
+    }
+
+
+    public void UpdateSliderValue()
+    {
+        Debug.Break();
+        healthSlider.value = EntityHealth.health / EntityHealth.maxHealth;
+    }
+
+    
+}
