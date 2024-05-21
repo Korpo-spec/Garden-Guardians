3658969dd2ccc6aa60fe534489f8735d76476306
Taqiqi <98962033+Taqiqi@users.noreply.github.com>
2024-05-21 12:22:19 +0200

    Beggining of ranged attack crow

--- Assets/Assets/Script/Entity/EnemyRangedAttackState.cs
+++ Assets/Assets/Script/Entity/EnemyRangedAttackState.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Script.PlayerMovement;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "States/EnemyRangedAttackState")]
@@ -7,15 +8,46 @@ public class EnemyRangedAttackState : State
 {
     public GameObject Projectile;
     
-    // Start is called before the first frame update
-    void Start()
+    private StateController _controller;
+    private EntityMovement _movement;
+    private Animator _animator;
+    private Collider[] _colliders;
+    private float _time;
+    
+    [SerializeField] private LayerMask _mask;
+    [SerializeField] private float _radius;
+    [SerializeField] private float _attackRange;
+    [SerializeField] private float _attackSpeed;
+    [SerializeField] private Faction _factionToAttack;
+    [SerializeField] private State _stateToTransistion;
+    [SerializeField] private AttackModule _attackModule;
+    [SerializeField] private Transform _target;
+    [SerializeField] private bool recover = true;
+    public override void OnEnter(StateController controller)
     {
-        
-    }
+        _controller = controller;
+        _colliders = new Collider[15];
+        _movement = controller.GetComponent<EntityMovement>();
+        _animator = controller.GetComponent<Animator>();
+        _time = _attackSpeed + 1;
+        int amount = Physics.OverlapSphereNonAlloc(_controller.transform.position, _radius, _colliders, _mask);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        for (int i = 0; i < amount; i++)
+        {
+            if (_colliders[i].TryGetComponent<EntityFaction>(out var faction))
+            {
+                if (faction.faction == _factionToAttack)
+                {
+                    _target = _colliders[i].gameObject.transform;
+                    _movement.SetDestinationTo(_target.position.RemoveY());
+                    return;
+                }
+            }
+            else
+            {
+                continue;
+            }
+        }
     }
+    
 }
