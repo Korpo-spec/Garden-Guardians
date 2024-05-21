73aeff3d0ce3386b58fca320890a01e34a323606
Taqiqi <98962033+Taqiqi@users.noreply.github.com>
2024-05-21 12:04:29 +0200

    Crow melee attack changes

---
+++ Assets/Assets/Script/Entity/EnemyRangedAttackState.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "States/EnemyRangedAttackState")]
+public class EnemyRangedAttackState : State
+{
+    public GameObject Projectile;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
