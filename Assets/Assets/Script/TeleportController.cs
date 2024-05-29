f2e05848f02dec6979710d2992d7e89da427b1a5
Breeake <123238518+Breeake@users.noreply.github.com>
2024-05-29 11:14:35 +0200

    Added functionality for reseting Npcs in teleport script

--- Assets/Assets/Script/TeleportController.cs
+++ Assets/Assets/Script/TeleportController.cs
@@ -8,12 +8,18 @@ public class TeleportController : MonoBehaviour
     public static event EventHandler <Vector3>PlayerTeleport;
 
     public Transform TeleportPosition;
+
+    [SerializeField] private ResetNpcs resetNpcs;
     
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             PlayerTeleport?.Invoke(this,TeleportPosition.position);
+            if (resetNpcs != null)
+            {
+                resetNpcs.ResetNpc();
+            }
         }
     }
 }
