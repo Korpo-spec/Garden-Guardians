41e860ebee5f12f1392b40e8523a2de0cf494519
Breeake <123238518+Breeake@users.noreply.github.com>
2024-06-03 14:40:37 +0200

    Audio for cinematic now fixed

--- Assets/Assets/Script/SkipCinematic.cs
+++ Assets/Assets/Script/SkipCinematic.cs
@@ -1,16 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class SkipCinematic : MonoBehaviour
 {
+    [SerializeField] private VideoPlayer videoPlayer;
+
+    private float videoTime;
+
+    private void Start()
+    {
+        videoTime = (float)videoPlayer.length;
+        Invoke("LoadNextScene", videoTime);
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.anyKeyDown)
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            LoadNextScene();
         }
+    }
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
